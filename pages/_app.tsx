05a3301b001f82020bea5dd085ab5ba7a35c5a13
petmatecl <petmatecl@gmail.com>
2025-10-17 18:38:01 -0300

    chore: move Header/Footer to components and fix imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-import 'react-day-picker/dist/style.css'
+import '../styles/globals.css';
+import 'react-day-picker/dist/style.css';
+import type { AppProps } from 'next/app';
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-zinc-900">
+    <>
       <Header />
-      <main className="flex-1">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
