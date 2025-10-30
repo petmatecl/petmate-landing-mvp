a65e3f66ae64b8dd6010f30937a5824908f1d657
petmatecl <petmatecl@gmail.com>
2025-10-30 20:28:27 -0300

    Home: hero con foto de Pexels

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
 };
 module.exports = nextConfig;
