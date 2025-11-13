8b82e25586eea9726a54a919d2d967e8467bc04e
petmatecl <petmatecl@gmail.com>
2025-11-13 19:52:40 -0300

    Evita duplicar Header y Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import 'react-day-picker/dist/style.css';
+import type { AppProps } from "next/app";
+import "../styles/globals.css"; // o la ruta que ya tenías para los estilos
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className="min-h-dvh flex flex-col bg-gray-50">
-      <Header />
-      <main className="flex-1">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
-  );
+export default function MyApp({ Component, pageProps }: AppProps) {
+  // Ya no renderizamos Header ni Footer aquí
+  return <Component {...pageProps} />;
 }
