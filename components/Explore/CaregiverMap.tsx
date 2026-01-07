4291006efd73070ead98c69d9beba6c7c8e8a635
petmatecl <petmatecl@gmail.com>
2026-01-07 10:26:34 -0300

    fix: import leaflet CSS in CaregiverMap to fix rendering

--- components/Explore/CaregiverMap.tsx
+++ components/Explore/CaregiverMap.tsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import { useEffect, useMemo } from "react";
 import Link from "next/link";
 
