85d7c4d8719c8206fab0651f474d98641955a273
petmatecl <petmatecl@gmail.com>
2026-01-07 09:29:49 -0300

    fix: use cdnjs for leaflet map markers to improve reliability

--- components/Explore/CaregiverMap.tsx
+++ components/Explore/CaregiverMap.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 useEffect(() => {
     delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
-        iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-        iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     });
 }, []);
 
